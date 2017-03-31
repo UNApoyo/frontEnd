8a185d325f5f2e75a790f0a1adb8068a289b726f
cclozanoj <cclozanoj@unal.edu.co>
2017-03-31 09:38:01 -0500

    fixing .css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HomeTitleComponent } from './home-title/home-title.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeTitleComponent
   ],
   imports: [
     BrowserModule,
