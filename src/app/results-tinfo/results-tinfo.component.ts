d641718b666c5a1b1eedaadaf422144542942cfd
Diana Navarrete <dcnavarreter@unal.edu.co>
2017-04-22 21:38:57 -0500

    mas bonito

--- src/app/results-tinfo/results-tinfo.component.ts
+++ src/app/results-tinfo/results-tinfo.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Http } from "@angular/http";
 
 @Component({
   selector: 'app-results-tinfo',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./results-tinfo.component.css']
 })
 export class ResultsTinfoComponent implements OnInit {
+  tipo1 = "Matematicas";
+  tipologias1;
+  tipo2 = "Probabilidad y Estadistica";
+  tipologias2;
+  constructor(private http: Http) {
+    http.get('http://localhost:3000/asignaturas/porcentaje_area/1/1/1.json')
+      .subscribe(res => this.tipologias1 = res.json());
+    http.get('http://localhost:3000/asignaturas/porcentaje_area/1/1/2.json')
+      .subscribe(res => this.tipologias2 = res.json());
+  }
 
-  constructor() { }
 
   ngOnInit() {
   }
-
-}
