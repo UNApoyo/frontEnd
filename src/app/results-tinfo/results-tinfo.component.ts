d12f10a3c23f3ce72200cbbad9eb266837562955
cclozanoj <cclozanoj@unal.edu.co>
2017-04-27 23:04:38 -0500

    inicio de lectura de historia

--- src/app/results-tinfo/results-tinfo.component.ts
+++ src/app/results-tinfo/results-tinfo.component.ts
@@ -22,3 +22,4 @@ export class ResultsTinfoComponent implements OnInit {
 
   ngOnInit() {
   }
+}
---
+++ src/js/gHistoria.js
@@ -0,0 +1,4 @@
+function historializar() {
+    var x = document.getElementById("historia").value;
+    document.getElementById("demo").innerHTML = x;
+}
